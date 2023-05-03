a5340de61d4cb882d1c537e55fb07eaa536ac8e2
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2023-05-04 03:24:30 +0530

    Removed some old project from Project component

--- src/Components/Projects/ProjectCardData.js
+++ src/Components/Projects/ProjectCardData.js
@@ -21,35 +21,7 @@ const ProjectCardData = [
         aboutProject: 'Frontend ecommerce website built using vanilla JavaScript',
         url1: 'https://rakesh-99.github.io/FrontEnd-Ecommerce-Project/',
         url2: 'https://github.com/Rakesh-99/FrontEnd-Ecommerce-Project'
-    },
-    {
-        imgSrc: ProjectImg3,
-        projectTitle: 'JavaScript Portfolio Website',
-        aboutProject: 'This Personal Portfoio website is built using JavaScript',
-        url1: 'https://rakesh-99.github.io/Personal-Portfolio/',
-        url2: 'https://github.com/Rakesh-99/Personal-Portfolio'
-    },
-    {
-        imgSrc: ProjectImg4,
-        projectTitle: 'Digital Watch',
-        aboutProject: 'Digital watch, a mini project built using Vanilla JavaScript',
-        url1: 'https://rakesh-99.github.io/Digital-Watch/',
-        url2: 'https://github.com/Rakesh-99/Digital-Watch'
-    },
-    {
-        imgSrc: ProjectImg5,
-        projectTitle: 'Age Calculator',
-        aboutProject: 'This project is basically made for calculating your current age',
-        url1: 'https://rakesh-99.github.io/Age-Calculator/',
-        url2: 'https://github.com/Rakesh-99/Age-Calculator'
-    },
-    {
-        imgSrc: ProjectImg6,
-        projectTitle: 'Calculator',
-        aboutProject: 'A mini calculator project built using JavaScript ',
-        url1: 'https://rakesh-99.github.io/Calculator/',
-        url2: 'https://github.com/Rakesh-99/Calculator'
-    },
+    }
 
 ];
 
