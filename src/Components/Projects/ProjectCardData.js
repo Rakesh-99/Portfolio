bf5bcb1df09a7bcea4467df46d5426e9fc3bec3c
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2023-06-13 18:09:56 +0530

    new project added in Project component

--- src/Components/Projects/ProjectCardData.js
+++ src/Components/Projects/ProjectCardData.js
@@ -1,10 +1,18 @@
 import ProjectImg1 from '../Assets/Project1.png';
 import ProjectImg2 from '../Assets/Project2.png';
 import ProjectImg3 from '../Assets/Project 3.png';
+import ProjectImg4 from '../Assets/Project4.png';
 
 
 
 const ProjectCardData = [
+    {
+        imgSrc: ProjectImg3,
+        projectTitle: 'MERN STACK CRUD APP',
+        aboutProject: 'This app is built using MongoDb, Express, React, and Node.This app is using a cloud server Mongodb atlas to perform the create, read, update and delete operations.',
+        url1: 'https://mern-crud-app-sigma.vercel.app/',
+        url2: 'https://github.com/Rakesh-99/MERN-CRUD-APP'
+    },
     {
         imgSrc: ProjectImg1,
         projectTitle: 'React Portfolio',
@@ -13,19 +21,21 @@ const ProjectCardData = [
         url2: 'https://github.com/Rakesh-99/React-Personal-Portfolio-'
     },
     {
+        imgSrc: ProjectImg4,
+        projectTitle: 'Weather App',
+        aboutProject: 'The Weather app built with react.The data are being consumed from the API called Open Weather Map Api. It has some features like current wind speed, pressure, humidity, dynamic icons according to current weather, sea level, visibility including city name and temperature.',
+        url1: 'https://weather-app-tau-dun.vercel.app/',
+        url2: 'https://github.com/Rakesh-99/Weather-App-React'
+    },
+    {
         imgSrc: ProjectImg2,
-        projectTitle: 'Ecommerce website',
-        aboutProject: 'Frontend ecommerce multi page website built using HTML5,CSS3 and,JavaScript. ',
+        projectTitle: 'Cara Clothes',
+        aboutProject: 'Cara clothes is a front-end Ecommerce multi page website built using HTML5,CSS3 and,JavaScript. ',
         url1: 'https://rakesh-99.github.io/FrontEnd-Ecommerce-Project/',
         url2: 'https://github.com/Rakesh-99/FrontEnd-Ecommerce-Project'
     },
-    {
-        imgSrc: ProjectImg3,
-        projectTitle: 'MERN STACK CRUD APP',
-        aboutProject: 'This app is built using MongoDb, Express, React, and Node.This app is using a cloud server Mongodb atlas to perform the create, read, update and delete operations.',
-        url1: 'https://mern-crud-app-sigma.vercel.app/',
-        url2: 'https://github.com/Rakesh-99/MERN-CRUD-APP'
-    }
+
+
 
 ];
 
