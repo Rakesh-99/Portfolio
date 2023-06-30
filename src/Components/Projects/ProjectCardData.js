438e137cfc91e7a3e555da425a08a562ae3ef897
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2023-06-30 08:38:05 +0530

    removed a project

--- src/Components/Projects/ProjectCardData.js
+++ src/Components/Projects/ProjectCardData.js
@@ -1,7 +1,7 @@
 import ProjectImg1 from '../Assets/Project1.png';
 import ProjectImg2 from '../Assets/Project2.png';
 import ProjectImg3 from '../Assets/Project 3.png';
-import ProjectImg4 from '../Assets/Project4.png';
+
 
 
 
@@ -19,13 +19,6 @@ const ProjectCardData = [
         aboutProject: 'Personal portfolio website built using React Js.',
         url1: 'https://react-personal-portfolio-coral.vercel.app/',
         url2: 'https://github.com/Rakesh-99/React-Personal-Portfolio-'
-    },
-    {
-        imgSrc: ProjectImg4,
-        projectTitle: 'Weather App',
-        aboutProject: 'The Weather app built with react.The data are being consumed from the API called Open Weather Map Api. It has some features like current wind speed, pressure, humidity, dynamic icons according to current weather, sea level, visibility including city name and temperature.',
-        url1: 'https://weather-app-tau-dun.vercel.app/',
-        url2: 'https://github.com/Rakesh-99/Weather-App-React'
     },
     {
         imgSrc: ProjectImg2,
