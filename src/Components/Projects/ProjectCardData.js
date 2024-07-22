96807b3016995d2956178e3701daed0e34395542
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-07-22 13:31:25 +0530

    update projects

--- src/Components/Projects/ProjectCardData.js
+++ src/Components/Projects/ProjectCardData.js
@@ -1,27 +1,17 @@
 import ProjectImg1 from '../Assets/Project1.png';
-import ProjectImg2 from '../Assets/Project2.png';
-import ProjectImg3 from '../Assets/Project 3.png';
-import noCodeImg from '../Assets/NoCode ProjectImg.png';
-import MernBlogImg from '../Assets/MernBlogImg.png';
-import WeatherAppImg from '../Assets/Project4.png';
+import ProjectImg2 from '../Assets/MernBlogImg.png';
+import ProjectImg3 from '../Assets/schoolManagement.png';
 
 
 
 
 const ProjectCardData = [
     {
-        imgSrc: MernBlogImg,
+        imgSrc: ProjectImg2,
         projectTitle: 'MERN STACK blog App',
         aboutProject: 'A fullstack blog app using MERN. It has some features include Admin panel,advance authentication using json web token,sorting,searching, password hashing, theme preference and much more.',
         url1: 'https://blogwizard.vercel.app/',
         url2: 'https://github.com/Rakesh-99/Fullstack-Blog-App'
-    },
-    {
-        imgSrc: ProjectImg3,
-        projectTitle: 'MERN STACK CRUD Operation',
-        aboutProject: 'This project is built using MongoDB, ExpressJs, ReactJs, and Node.js, with  MongoDB Atlas serving as the cloud - based database for performing Create, Read, Update, and Delete(CRUD) operations',
-        url1: 'https://mern-crud-app-sigma.vercel.app/',
-        url2: 'https://github.com/Rakesh-99/MERN-CRUD-APP'
     },
     {
         imgSrc: ProjectImg1,
@@ -31,25 +21,11 @@ const ProjectCardData = [
         url2: 'https://github.com/Rakesh-99/React-Personal-Portfolio-'
     },
     {
-        imgSrc: WeatherAppImg,
-        projectTitle: 'Weather App using React',
-        aboutProject: 'I have developed a React - based weather app that provides real-time information, including current wind speed, atmospheric pressure,humidity, dynamic weather icons, sea level, visibility, city name, and temperature',
-        url1: 'https://weather-app-tau-dun.vercel.app/',
-        url2: 'https://github.com/Rakesh-99/Weather-App-React'
-    },
-    {
-        imgSrc: noCodeImg,
-        projectTitle: 'Bubble No-Code project',
-        aboutProject: 'A no-code food-recipe app built using Bubble.io.',
-        url1: 'https://recipezone.bubbleapps.io/version-test/auth?v=login',
-        url2: '#'
-    },
-    {
-        imgSrc: ProjectImg2,
-        projectTitle: 'Cara Clothes',
-        aboutProject: 'Cara Clothes is a multi - page eCommerce website with a front-end design crafted using HTML5, CSS3, and JavaScript',
-        url1: 'https://rakesh-99.github.io/FrontEnd-Ecommerce-Project/',
-        url2: 'https://github.com/Rakesh-99/FrontEnd-Ecommerce-Project'
+        imgSrc: ProjectImg3,
+        projectTitle: 'School Managament System',
+        aboutProject: 'Developed a core Java-based school management system implementing object oriented principles. The system tracks students and teachers, including total earnings from students and total salary expenses for teachers.',
+
+        url2: 'https://github.com/Rakesh-99/School-management-system'
     },
 ];
 
